835ee8b21267aa41d2aa802a808b5c5ea8d3581e
rutuja_mali <rutujamali111@gmail.com>
2022-07-28 00:34:42 +0530

    Start the game

---
+++ Game.java
@@ -0,0 +1,10 @@
+
+public class Game {
+	private static final int Startposition =0;
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to the snake and ladder game...");
+		System.out.println("Starting position: "+Startposition);
+	}
+
+}
