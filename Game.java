895b939035ec8501a0e22d961812bc6d6c83bfe0
rutuja_mali <rutujamali111@gmail.com>
2022-07-28 00:46:25 +0530

    winning condition

--- Game.java
+++ Game.java
@@ -1,13 +1,16 @@
 
 public class Game {
-		private static final int Startposition =0;
-		private static final int No_Play=0;
-		private static final int Ladder=1;
-		private static final int snake=2;
-		public static void main(String[] args) {
-			System.out.println("Welcome to the snake and ladder game...");
-			System.out.println("Starting position: "+Startposition);
-			int End_position=0;
+	private static final int Startposition =0;
+	private static final int No_Play=0;
+	private static final int Ladder=1;
+	private static final int snake=2;
+	private static final int Winning_position=100;
+	public static void main(String[] args) {
+		System.out.println("Welcome to the snake and ladder game...");
+		System.out.println("Starting position: "+Startposition);
+		int End_position=0;
+		while(End_position!=Winning_position)
+		{
 			int die=(int)(Math.floor(Math.random()*10)%6)+1;
 			System.out.println("you got:"+die);
 			int play=(int)Math.floor(Math.random()*10)%3;
@@ -19,12 +22,13 @@ public class Game {
 					break;
 				case snake:
 					End_position-=die;
+					if(End_position<0)
+						End_position=0;
 					break;
 				default:
 					End_position+=0;
 			}
 			System.out.println("Your position is : "+End_position);
-				
-}
-
+		}	
+	}
 }
