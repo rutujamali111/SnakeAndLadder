77ef8b1f724d518efe714d0899e9a2c32d0108ca
rutuja_mali <rutujamali111@gmail.com>
2022-07-28 00:58:24 +0530

    Two Player game

--- Game.java
+++ Game.java
@@ -5,35 +5,81 @@ public class Game {
 	private static final int Ladder=1;
 	private static final int snake=2;
 	private static final int Winning_position=100;
+	
 	public static void main(String[] args) {
+		 boolean Player1=true;
 		System.out.println("Welcome to the snake and ladder game...");
-		System.out.println("Starting position: "+Startposition);
-		int End_position=0;
+		System.out.println("Starting position of player1 and player2 is : "+Startposition);
+		
+		int End_position1=0;
+		int End_position2=0;
 		int total_Throws=0;
-		while(End_position!=Winning_position)
+		while(End_position1!=Winning_position && End_position2!=Winning_position)
 		{
 			int die=(int)(Math.floor(Math.random()*10)%6)+1;
-			System.out.println("you got:"+die);
-			total_Throws++;
 			int play=(int)Math.floor(Math.random()*10)%3;
-			System.out.println("Your play is:"+play);
-			switch(play)
+		
+			if(Player1)
 			{
-				case Ladder:
-					End_position+=die;
-					if(End_position>100)
-						End_position-=die;
-					break;
-				case snake:
-					End_position-=die;
-					if(End_position<0)
-						End_position=0;
-					break;
-				default:
-					End_position+=0;
+			
+				System.out.println("Player1:"+die);
+				total_Throws++;
+				
+				System.out.println(" play of player1 is:"+play);
+				switch(play)
+				{
+					case Ladder:
+						End_position1+=die;
+						if(End_position1>100)
+							End_position1-=die;
+						break;
+					case snake:
+						End_position1-=die;
+						if(End_position1<0)
+							End_position1=0;
+						break;
+					default:
+						End_position1+=0;
+				}
+				System.out.println("Player1 position is : "+End_position1);
+				if(play==Ladder)
+					Player1=true;
+				else
+					Player1=false;
 			}
-			System.out.println("Your position is : "+End_position);
+			else
+			{
+				System.out.println("Player2:"+die);
+				total_Throws++;
+				
+				System.out.println(" play of player2 is:"+play);
+				switch(play)
+				{
+					case Ladder:
+						End_position2+=die;
+						if(End_position2>100)
+							End_position2-=die;
+						break;
+					case snake:
+						End_position2-=die;
+						if(End_position2<0)
+							End_position2=0;
+						break;
+					default:
+						End_position2+=0;
+				}
+				System.out.println("Player2 position is : "+End_position2);
+				if(play==Ladder)
+					Player1=false;
+				else
+					Player1=true;
+				
+			}
 		}
+		if(End_position1==Winning_position)
+			System.out.println("Player1 is winner...");
+		else
+			System.out.println("Player2 is winner...");
 		System.out.println("You throws the dice "+total_Throws+" times");
 	}
 }
