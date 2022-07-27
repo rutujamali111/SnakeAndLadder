031ddf771df93888c88f76ac7caa550556615c05
rutuja_mali <rutujamali111@gmail.com>
2022-07-28 00:54:15 +0530

    NUmber of Throws

--- Game.java
+++ Game.java
@@ -5,15 +5,16 @@ public class Game {
 	private static final int Ladder=1;
 	private static final int snake=2;
 	private static final int Winning_position=100;
-	public static void main(String[] args) 
-	{
+	public static void main(String[] args) {
 		System.out.println("Welcome to the snake and ladder game...");
 		System.out.println("Starting position: "+Startposition);
 		int End_position=0;
+		int total_Throws=0;
 		while(End_position!=Winning_position)
 		{
 			int die=(int)(Math.floor(Math.random()*10)%6)+1;
 			System.out.println("you got:"+die);
+			total_Throws++;
 			int play=(int)Math.floor(Math.random()*10)%3;
 			System.out.println("Your play is:"+play);
 			switch(play)
@@ -33,5 +34,6 @@ public class Game {
 			}
 			System.out.println("Your position is : "+End_position);
 		}
+		System.out.println("You throws the dice "+total_Throws+" times");
 	}
 }
